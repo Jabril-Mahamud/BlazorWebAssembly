8528d57600e778d99982baecbe231f8c07ff4886
Jabril Mahamud (personal account) <59336921+Jabril-Mahamud@users.noreply.github.com>
2024-08-31 20:01:56 +0100

    added a change so it uses the app service and not local host

--- Program.cs
+++ Program.cs
@@ -2,18 +2,32 @@ using BlazorWebAssembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Configure HttpClient for general use
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
 
+// Configure HttpClient for ProductService
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://blazorwebassembly20240831193600.azurewebsites.net/")
+});
+
+// Register Radzen's NotificationService
 builder.Services.AddScoped<NotificationService>();
 
+// Configure Azure AD authentication
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    // Configure additional authentication options if needed
 });
 
 await builder.Build().RunAsync();
