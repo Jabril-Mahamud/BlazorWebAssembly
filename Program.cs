4e07e7e7d1e3e2d7073f8c1a35a5a756c5706549
Jabril Mahamud (personal account) <59336921+Jabril-Mahamud@users.noreply.github.com>
2024-08-31 20:13:46 +0100

    removed old one

--- Program.cs
+++ Program.cs
@@ -8,11 +8,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Configure HttpClient for general use
-builder.Services.AddScoped(sp => new HttpClient
-{
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
 
 // Configure HttpClient for ProductService
 builder.Services.AddScoped(sp => new HttpClient
